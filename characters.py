961d551c61acab85cc7426a787e44ad648e03b44
alieva04042008-jpg <alieva04042008@gmail.com>
2025-11-18 16:21:52 +0100

    Change characters

--- characters.py
+++ characters.py
@@ -1,8 +1,8 @@
 characters = [
     {"name": "Alice", "description": "Has 28 cats", "image": "images/alice.webp"},
-    {"name": "Bob", "description": "Enjoys sunbathing", "image": "images/bob.jpg"},
-    {"name": "Clara", "description": "Has her own business", "image": "images/clara.avif"},
-    {"name": "Simon", "description": "Loves to play soccer", "image": "images/simon.png"},
-    {"name": "Kamila", "description": "Likes hanging out", "image": "images/kamila.png"},
+    {"name": "Bob", "description": "Enjoys sunbathing", "image": "images/bob.webp"},
+    {"name": "Clara", "description": "Has her own business", "image": "images/clara.webp"},
+    {"name": "Simon", "description": "Loves to play soccer", "image": "images/simon.webp"},
+    {"name": "Kamila", "description": "Likes hanging out", "image": "images/kamila.jpeg"},
     {"name": "Danial", "description": "Watches to many movies", "image": "images/danial.png"},
 ]
